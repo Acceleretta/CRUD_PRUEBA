cc2415bc9f655558dfe5b993eb7a123f5fd6cbb5
Gustavo León Landeros <gleonlanderos@gmail.com>
2025-05-19 16:51:52 -0600

    Agregar archivos de proyecto.

---
+++ Models/Empleado.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CRUD_PRUEBA.Models;
+
+public partial class Empleado
+{
+    public string? Nombre { get; set; }
+
+    public string Nue { get; set; } = null!;
+
+    public string? Correo { get; set; }
+
+    public string? Puesto { get; set; }
+}
